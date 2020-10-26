2a1722c38b2a270e086eca67679e4cf6c2045382
OlanSer <57589024+olanser@users.noreply.github.com>
2020-10-26 17:34:04 +0200

    Upd find and replase, fix bag

--- window_find_replace.cpp
+++ window_find_replace.cpp
@@ -17,10 +17,32 @@ window_Find_Replace::~window_Find_Replace()
 
 void window_Find_Replace::on_button_Find_clicked()
 {
-    qobject_cast<MainWindow*>(this->parent())->windowFind(ui->text_Find->text());
+    bool isRegex = false;
+    QTextDocument::FindFlags flag;
+
+    if (ui->cRegex->checkState())
+        isRegex = true;
+    if (ui->cFindBackward->checkState())
+        flag = flag| QTextDocument::FindBackward;
+    if (ui->cFindCaseSensitively->checkState())
+        flag = flag| QTextDocument::FindCaseSensitively;
+    if (ui->cFindWholeWords->checkState())
+        flag = flag| QTextDocument::FindWholeWords;
+    qobject_cast<MainWindow*>(this->parent())->windowFind(ui->text_Find->text(), isRegex, flag);
 }
 
 void window_Find_Replace::on_button_Replace_clicked()
 {
-    qobject_cast<MainWindow*>(this->parent())->windowReplace(ui->text_Find->text(), ui->text_Replace->text());
+    bool isRegex = false;
+    QTextDocument::FindFlags flag;
+
+    if (ui->cRegex->checkState())
+        isRegex = true;
+    if (ui->cFindBackward->checkState())
+        flag = flag| QTextDocument::FindBackward;
+    if (ui->cFindCaseSensitively->checkState())
+        flag = flag| QTextDocument::FindCaseSensitively;
+    if (ui->cFindWholeWords->checkState())
+        flag = flag| QTextDocument::FindWholeWords;
+    qobject_cast<MainWindow*>(this->parent())->windowReplace(ui->text_Find->text(), ui->text_Replace->text(), isRegex, flag);
 }
